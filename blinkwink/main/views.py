38a0383a3a38e84a23340102239330af4954f5e0
Chris Shepard <imajunryou@gmail.com>
2017-11-28 20:33:27 -0700

    Add economic history project bits

--- blinkwink/main/views.py
+++ blinkwink/main/views.py
@@ -8,3 +8,7 @@ def index():
 @main.route("reasoning")
 def reasoning():
     return render_template("reasoning_project.html")
+
+@main.route("econ")
+def econ():
+    return render_template("econ_project.html")
--- test_blinkwink.py
+++ test_blinkwink.py
@@ -21,5 +21,9 @@ class TestTestCase(TestCase):
         response = self.client.get(url_for("main.reasoning"))
         self.assertEqual(response.status_code, 200)
 
+    def test_can_connect_to_econ_page(self):
+        response = self.client.get(url_for("main.econ"))
+        self.assertEqual(response.status_code, 200)
+
 if __name__ == "__main__":
     unittest.main()
